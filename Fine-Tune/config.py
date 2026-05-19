18498dbc1dad8d5677cdc0100d0b15473316e393
Debasmita Panda <151456444+debby123p@users.noreply.github.com>
2026-05-19 22:01:38 +0530

    Update config.py

--- Fine-Tune/config.py
+++ Fine-Tune/config.py
@@ -2,7 +2,7 @@ import os
 import re
 
 # Central mapping model aliases to HuggingFace IDs
-MODEL_ZOO = {
+MODEL_LIST = {
     "qwen-8b": "Qwen/Qwen3-8B",
     "qwen-32b": "Qwen/Qwen3-32B",
     "dseek-14b": "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B",
